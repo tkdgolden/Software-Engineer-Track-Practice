6a750565d869bde74b298d4edb27187a5f715944
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-22 22:23:26 -0600

    js remove even function

--- js/challenges/001_EvenNumbers/index.mjs
+++ js/challenges/001_EvenNumbers/index.mjs
@@ -1,3 +1,12 @@
 export default (arr) => {
-
+    const newArray = [];
+    arr.forEach(element => {
+        if (isNaN(element)) {
+            throw new TypeError('The array should contain only numbers.')
+        };
+        if (element % 2 !== 0) {
+            newArray.push(element)
+        };
+    });
+    return newArray;
 };
