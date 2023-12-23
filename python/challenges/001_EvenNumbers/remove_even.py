e87cfe9fe62ed14c02946c30a823f7d9105a99b0
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-22 22:25:57 -0600

    comment

--- python/challenges/001_EvenNumbers/remove_even.py
+++ python/challenges/001_EvenNumbers/remove_even.py
@@ -1,4 +1,6 @@
 def remove_even_numbers(lst):
+    """ Returns a new array containing only the odd numbers from the original array, if any. """
+    
     new_array = []
     for each in lst:
         if each % 2 != 0:
