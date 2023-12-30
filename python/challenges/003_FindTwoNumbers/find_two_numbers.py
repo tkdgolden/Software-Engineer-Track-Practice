6968c8b1262d8fffe418f6d6087e98013d43d961
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-30 12:01:43 -0600

    can't remove elements from the array, better memory use by calling for index on return line

--- python/challenges/003_FindTwoNumbers/find_two_numbers.py
+++ python/challenges/003_FindTwoNumbers/find_two_numbers.py
@@ -1,14 +1,8 @@
 def find_two_numbers(nums, target):
     """ returns an array containing the indices of the two numbers in the given array that add up to the target """
 
-    indices = []
-
     for each in nums:
         if each < target:
             remainder = (target - each)
             if remainder in nums:
-                indices.append(nums.index(remainder))
-                indices.append(nums.index(each))
-        nums.remove(each)
-
-    return indices
+                return [nums.index(remainder), nums.index(each)]
