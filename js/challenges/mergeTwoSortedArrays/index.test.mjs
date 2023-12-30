59459a2c5204d7cf02379e01ddcdb147e9378b2f
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-30 11:04:58 -0600

    merge two js function, error with tests

---
+++ js/challenges/mergeTwoSortedArrays/index.test.mjs
@@ -0,0 +1,31 @@
+import { default as mergeTwoSortedArrays } from './index.mjs';
+
+describe('mergeTwoSortedArrays function', function () {
+
+    it('merges two typical arrays', function () {
+        expect(mergeTwoSortedArrays([1, 3, 6], [2, 4, 5])).toEqual([1, 2,3, 4, 5, 6]);
+    });
+
+    it('merge includes duplicates', function () {
+        expect(mergeTwoSortedArrays([1, 2, 3], [1, 2, 3])).toEqual([1, 1, 2, 2, 3, 3]);
+    });
+
+    it('returns array when one array is empty', function () {
+        expect(mergeTwoSortedArrays([1, 2, 3], [])).toEqual([1, 2, 3]);
+    });
+
+    it('returns array when inputs are different lengths', function () {
+        expect(mergeTwoSortedArrays([1, 5], [2, 3, 4, 5, 6, 9, 12, 14])).toEqual([1, 2, 3, 4, 5, 5, 6, 9, 12, 14]);
+    });
+
+    it('returns empty array when both inputs are empty', function () {
+        expect(mergeTwoSortedArrays([], [])).toEqual([]);
+    });
+
+    it('throws an error for missing input', function() {
+        expect(function () {
+            mergeTwoSortedArrays([1, 3, 4]);
+        }).toThrowError();
+    });
+
+});
