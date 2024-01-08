8565bcb679f3b4ef8983c5e62078d81054ec3f7a
Olivia Golden <olivia.golden.w@gmail.com>
2024-01-08 15:44:52 -0600

    using counters instead of shift or splice

--- js/challenges/002_MergeTwoSortedArrays/index.mjs
+++ js/challenges/002_MergeTwoSortedArrays/index.mjs
@@ -7,17 +7,26 @@
 export function mergeTwoSortedArrays(arr1, arr2) {
     let newArray = [];
 
-    while((arr1.length > 0) && (arr2.length > 0)) {
-        if (arr1[0] < arr2[0]) {
-            newArray.push(arr1.splice(0, 1)[0]);
+    let index1 = 0;
+    let index2 = 0;
+    while((index1 < arr1.length) && (index2 < arr2.length)) {
+        if (arr1[index1] < arr2[index2]) {
+            newArray.push(arr1[index1]);
+            index1++;
         }
         else {
-            newArray.push(arr2.splice(0, 1)[0]);
+            newArray.push(arr2[index2]);
+            index2++;
         }
     }
-
-    newArray.push.apply(newArray, arr1);
-    newArray.push.apply(newArray, arr2);
+    while(index1 < arr1.length) {
+        newArray.push(arr1[index1]);
+        index1++;
+    }
+    while(index2 < arr2.length) {
+        newArray.push(arr2[index2]);
+        index2++;
+    }
 
     return newArray;
 }
