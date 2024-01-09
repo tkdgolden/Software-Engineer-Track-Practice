8f01a416dfd39ea6ef7e42096b1b915e84230053
Olivia Golden <olivia.golden.w@gmail.com>
2024-01-09 16:21:59 -0600

    update js tests

--- js/challenges/004_ProductsExceptSelf/index.test.mjs
+++ js/challenges/004_ProductsExceptSelf/index.test.mjs
@@ -7,15 +7,15 @@ describe('productsExceptSelf function', function () {
     });
 
     it('finds products with one zero', function () {
-        expect(productsExceptSelf([1, 2, 3])).toEqual([6, 3, 2]);
+        expect(productsExceptSelf([1, 2, 3, 0])).toEqual([0, 0, 0, 6]);
     });
 
     it('finds products with two zeroes', function () {
-        expect(productsExceptSelf([1, 2, 3])).toEqual([6, 3, 2]);
+        expect(productsExceptSelf([0, 1, 2, 3, 0])).toEqual([0, 0, 0, 0, 0]);
     });
 
     it('returns empty array, given empty array', function () {
-        expect(productsExceptSelf([1, 2, 3])).toEqual([6, 3, 2]);
+        expect(productsExceptSelf([])).toEqual([]);
     });
 
     it('returns empty array, given a single item', function () {
