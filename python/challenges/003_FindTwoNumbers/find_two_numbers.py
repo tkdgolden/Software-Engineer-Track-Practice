79cf7877618910e5996e4af7fce4d8b04cdfc0a6
Olivia Golden <olivia.golden.w@gmail.com>
2024-01-09 16:55:08 -0600

    linting

--- python/challenges/003_FindTwoNumbers/find_two_numbers.py
+++ python/challenges/003_FindTwoNumbers/find_two_numbers.py
@@ -1,9 +1,13 @@
 def find_two_numbers(nums, target):
-    """ returns an array containing the indices of the two numbers in the given array that add up to the target """
+    """ returns an array containing the indices of the two 
+    numbers in the given array that add up to the target """
 
+    indices = []
     for each in nums:
         if each < target:
-            remainder = (target - each)
+            remainder = target - each
             if remainder in nums:
-                return [nums.index(remainder), nums.index(each)]
+                indices = [nums.index(remainder), nums.index(each)]
+    
+    return indices
             
