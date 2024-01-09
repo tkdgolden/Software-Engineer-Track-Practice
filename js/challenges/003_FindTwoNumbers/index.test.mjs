79cb5d5723199e3ae3dd5f589633e2102b9c3a7d
Olivia Golden <olivia.golden.w@gmail.com>
2024-01-09 14:21:43 -0600

    test

--- js/challenges/003_FindTwoNumbers/index.test.mjs
+++ js/challenges/003_FindTwoNumbers/index.test.mjs
@@ -1,8 +1,8 @@
-import { findTwoNumbers } from "./index.mjs";
+
 
 describe('findTwoNumbers function', function () {
     it('test', function () {
-        a = findTwoNumbers([2, 7, 11, 15], 9)
-        expect(a).toEqual(a);
+        let a = 1 + 2;
+        expect(a).toEqual(3);
     });
 });
