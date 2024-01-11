daf53a364c0956b212d9a7f9ed6fbbe42b9a61ea
Olivia Golden <olivia.golden.w@gmail.com>
2024-01-11 12:24:05 -0600

    dictionary for more efficient searching

--- python/challenges/003_FindTwoNumbers/find_two_numbers.py
+++ python/challenges/003_FindTwoNumbers/find_two_numbers.py
@@ -2,12 +2,13 @@ def find_two_numbers(nums, target):
     """ returns an array containing the indices of the two 
     numbers in the given array that add up to the target """
 
-    indices = []
+    items = {}
+    counter = 0
     for each in nums:
-        if each < target:
-            remainder = target - each
-            if remainder in nums:
-                indices = [nums.index(remainder), nums.index(each)]
-
-    return indices
+        remainder = target - each
+        if remainder in items:
+            return [items[remainder], counter]
+        else:
+            items[each] = counter
+        counter += 1
             
